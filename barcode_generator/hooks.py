c651e130673bbb862445b8c62664286741c6a2dd
KasuniB <113013082+KasuniB@users.noreply.github.com>
2025-05-21 11:50:41 +0300

    Update hooks.py

--- barcode_generator/hooks.py
+++ barcode_generator/hooks.py
@@ -96,11 +96,11 @@ doc_events = {
         "on_submit": "barcode_generator.utils.api.stock_entry_after_submit"
     }
 }
-#doc_events = {
-    #"POS Closing Entry": {
-       # "on_submit": "barcode_generator.barcode_generator.doctype.item_daily_tracker.item_daily_tracker.handle_pos_closing_submit"
-   # }
-#}
+doc_events = {
+    "POS Closing Entry": {
+       # "on_submit": "barcode_generator.doctype.item_daily_tracker.item_daily_tracker.handle_pos_closing_submit"
+   }
+}
 
 # Scheduled Tasks
 # ---------------
