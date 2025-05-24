4ba36a543bb9f64ab946e4453e88d7601cdc6dd8
KasuniB <113013082+KasuniB@users.noreply.github.com>
2025-05-24 17:20:45 +0300

    update

--- barcode_generator/barcode_generator/doctype/pos_invoice/__init__.py
+++
@@ -1,6 +0,0 @@
-import frappe
-from barcode_generator.doctype.pos_invoice import pos_invoice as custom_pos_invoice
-import erpnext.accounts.report.general_ledger.general_ledger
-
-# Replace the original execute function with your custom one
-erpnext.accounts.doctype.pos_invoice.pos_invoice = custom_pos_invoice
