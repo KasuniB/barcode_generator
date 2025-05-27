23d81c9c06407e490d1a388abefe38fbd86cd7b1
KasuniB <113013082+KasuniB@users.noreply.github.com>
2025-05-27 09:50:17 +0300

    update

--- barcode_generator/barcode_generator/doctype/item_daily_tracker/item_daily_tracker.py
+++ barcode_generator/barcode_generator/doctype/item_daily_tracker/item_daily_tracker.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2025, YourCompany and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe import _
 from frappe.model.document import Document
--- barcode_generator/barcode_generator/doctype/item_daily_tracker_item/item_daily_tracker_item.py
+++ barcode_generator/barcode_generator/doctype/item_daily_tracker_item/item_daily_tracker_item.py
@@ -1,7 +1,3 @@
-# Copyright (c) 2025, YourCompany and contributors
-# For license information, please see license.txt
-
-# import frappe
 from frappe.model.document import Document
 
 class ItemDailyTrackerItem(Document):
